a1183374b8b76721757ffb36077b346fe44313b5
fengmingming <1053931671@qq.com>
2025-05-06 18:05:00 +0800

    fix bug

--- src/main/java/boluo/common/cache/DefaultKeyConverter.java
+++ src/main/java/boluo/common/cache/DefaultKeyConverter.java
@@ -2,10 +2,14 @@ package boluo.common.cache;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class DefaultKeyConverter implements KeyConverter{
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    static {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 
     @Override
     public String apply(Object o) {
